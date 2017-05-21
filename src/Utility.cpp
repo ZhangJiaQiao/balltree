4a0292a08db9a3b6f2d3703698c6142fc31d31a0
zhangjiaqiao <1254860908@qq.com>
2017-05-21 22:25:32 +0800

    修好了Utility.cpp中的read_data函数，把原来没用读进的id序号读进data数组里。

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -35,15 +35,15 @@ bool read_data(int n, int d, float** &data, const char* file_name) {
 		return false;
 	}
 
-	int id;
+	//int id;
 	data = new float*[n];
 	for (int i = 0; i < n; i++) {
 		data[i] = new float[d];
-		fscanf(fin, "%d", &id);
+		//fscanf(fin, "%d", &id);
 		for (int j = 0; j < d; j++) {
 			fscanf(fin, "%f", &data[i][j]);
 		}
-        printf("Data %d loaded.\n", id);
+        printf("Data %d loaded.\n", data[i][0]);
 	}
 
 	printf("Finish reading %s\n", file_name);
